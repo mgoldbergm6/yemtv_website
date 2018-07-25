f10985514083cd83e22828e9c1e5c7e4b5f69034
Michael <goldbergm6@gmail.com>
2018-07-25 15:37:29 -0500

    adding bootstrap

--- wp-content/themes/yemgaming/functions.php
+++ wp-content/themes/yemgaming/functions.php
@@ -1,3 +1,25 @@
 <?php
 
+/*--- Custom Header Function --- */
+
+add_theme_support( 'custom-header' );
+
+$headerzone = array(
+	'default-image'          => '',
+	'width'                  => 100,
+	'height'                 => 550,
+	'flex-height'            => false,
+	'flex-width'             => true,
+	'uploads'                => true,
+	'random-default'         => false,
+	'header-text'            => true,
+	'default-text-color'     => '',
+	'wp-head-callback'       => '',
+	'admin-head-callback'    => '',
+	'admin-preview-callback' => '',
+);
+
+add_theme_support( 'custom-header', $headerzone );
+
+
  ?>
